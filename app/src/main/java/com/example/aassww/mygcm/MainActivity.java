020c26f7d19940892f615436e86e19b239fcf5fe
aassw <aasswqqw11@knu.ac.kr>
2017-01-20 14:34:59 +1100

    01-20 14:34 push 메세지가 오면 지정된 번호들로 SMS 보내는 것 까지 완료

--- app/src/main/java/com/example/aassww/mygcm/MainActivity.java
+++ app/src/main/java/com/example/aassww/mygcm/MainActivity.java
@@ -1,23 +1,32 @@
 package com.example.aassww.mygcm;
 
+import android.*;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
 
+    public final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
+    public final int MY_PERMISSION_REQUEST_STORAGE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        checkPermission();
+
         Button getTokenButton = (Button) findViewById(R.id.checkTokenButton);
-        Button reviseButton = (Button)findViewById(R.id.reviseNumber) ;
+        Button reviseButton = (Button) findViewById(R.id.reviseNumber);
         getTokenButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
                 service.onTokenRefresh();
             }
         });
-        reviseButton.setOnClickListener(new View.OnClickListener(){
+        reviseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, Checknumber.class);
@@ -37,5 +46,49 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    private void checkPermission() {
+        if (checkSelfPermission(android.Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED
+                || checkSelfPermission(android.Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (shouldShowRequestPermissionRationale(android.Manifest.permission.SEND_SMS)) {
+                // Explain to the user why we need to write the permission.
+                Toast.makeText(this, "Read/Write external storage", Toast.LENGTH_SHORT).show();
+            }
+
+            requestPermissions(new String[]{android.Manifest.permission.SEND_SMS, android.Manifest.permission.SEND_SMS},
+                    MY_PERMISSION_REQUEST_STORAGE);
+
+            // MY_PERMISSION_REQUEST_STORAGE is an
+            // app-defined int constant
+
+        } else {
+        }
+    }
 }
--- app/src/main/java/com/example/aassww/mygcm/SendMessage.java
+++ app/src/main/java/com/example/aassww/mygcm/SendMessage.java
@@ -1,22 +1,107 @@
 package com.example.aassww.mygcm;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.Manifest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class SendMessage extends AppCompatActivity {
 
+    public String data = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_message);
 
         Intent intent = getIntent();
-        String data = intent.getStringExtra("message");
+        data = intent.getStringExtra("message");
+
+        phpDown task;
+        task = new phpDown();
+        task.execute();
 
         TextView textView = (TextView)findViewById(R.id.textView);
         textView.setText(data);
 
+    }
+
+    private void send(String num, String ms) {
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(num, null, ms, null, null);
+    }
+    private class phpDown extends AsyncTask<String, Integer, String> {
+        @Override
+        protected String doInBackground(String... poi_id) {
+            StringBuilder jsonHtml = new StringBuilder();
+            try {
+                // 연결 url 설정String minorid = (String) params[0];
+
+                String link = "http://tripjuvo.ivyro.net/fcm/listPhonenumber.php";
+                //    String data = URLEncoder.encode("poi_id", "UTF-8") + "=" + URLEncoder.encode(poi_id[0], "UTF-8");
+                URL url = new URL(link);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setDoOutput(true);
+                OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+                //   wr.write(data);
+                wr.flush();
+
+                // 커넥션 객체 생성
+                // 연결되었으면.
+                if (conn != null) {
+                    conn.setConnectTimeout(10000);
+                    // 연결되었음 코드가 리턴되면.
+                    if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+                        for (; ; ) {
+                            // 웹상에 보여지는 텍스트를 라인단위로 읽어 저장.
+                            String line = br.readLine();
+                            if (line == null) break;
+                            // 저장된 텍스트 라인을 jsonHtml에 붙여넣음
+                            jsonHtml.append(line + "\n");
+                        }
+                        br.close();
+                    }
+                    conn.disconnect();
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            return jsonHtml.toString();
+        }
+        protected void onPostExecute(String str) {
+            try {
+                JSONObject root = new JSONObject(str);
+                JSONArray ja = root.getJSONArray("results"); //get the JSONArray which I made in the php file. the name of JSONArray is "results"
+
+                for (int i = 0; i < ja.length(); i++) {
+                    JSONObject jo = ja.getJSONObject(i);
+
+                    String phone = jo.getString("phoneNumber");
+                    Log.i("SENDSMS",data);
+                    send(phone,data);
+
+                }
+            }
+            catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
